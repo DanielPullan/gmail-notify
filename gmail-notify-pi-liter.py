cd22e206eb7b55c2121599d793cf81a7279df603
Dan <dandandrumsgm@gmail.com>
2014-10-27 17:04:46 +0000

    added GPIO mapping and bulk of average man code
    also added thanks to average man

--- gmail-notify-pi-liter.py
+++ gmail-notify-pi-liter.py
@@ -7,10 +7,14 @@
 ## Twitter: @soundsarenoisy
 ## Github: /soundsarenoisy
 
+## Thanks to "Average Man" for the GPIO Mapping.
+
 
 ## imports
 import time
 import feedparser
+import os 
+import RPi.GPIO as GPIO  
 
 ## global variables for gmail
 ## username = raw_input('Your gmail username is:')
@@ -21,6 +25,30 @@ mail = int(feedparser.parse("https://" + username + ":" + password +
 contact = int(feedparser.parse("https://" + username + ":" + password +
 "@mail.google.com/gmail/feed/atom/LABEL-NAME")["feed"] ["fullcount"])
 
-## everything else
-## empty for now
-
+# Define PiLITEr to GPIO mapping  
+LED1 = 7  
+LED2 = 11  
+LED3 = 13   
+LED4 = 12  
+LED5 = 15  
+LED6 = 16  
+LED7 = 18  
+LED8 = 22  
+  
+# Main program  
+def main():  
+ GPIO.setmode(GPIO.BCM) # Use BCM GPIO numbers  
+ GPIO.setup(LED1, GPIO.OUT) #Set GPIO pin to output (to 'give' power)  
+ GPIO.setup(LED2, GPIO.OUT) #Set GPIO pin to output (to 'give' power)  
+ GPIO.setup(LED3, GPIO.OUT) #Set GPIO pin to output (to 'give' power)  
+ GPIO.setup(LED4, GPIO.OUT) #Set GPIO pin to output (to 'give' power)  
+ GPIO.setup(LED5, GPIO.OUT) #Set GPIO pin to output (to 'give' power)  
+ GPIO.setup(LED6, GPIO.OUT) #Set GPIO pin to output (to 'give' power)  
+ GPIO.setup(LED7, GPIO.OUT) #Set GPIO pin to output (to 'give' power)  
+ GPIO.setup(LED8, GPIO.OUT) #Set GPIO pin to output (to 'give' power)  
+         
+ # Turn on LED 1  
+ GPIO.output(LED1) == True  
+  
+if __name__ == '__main__':  
+ main() 
