f3edc52d08c1bfc782b12587cf9507d9cf10ba16
Dan <dandandrumsgm@gmail.com>
2014-11-06 15:18:17 +0000

    Piglow version gmail variables updated
    Added prompts to username and password. removed username 789. No changes
    to other versions.

--- gmail-notify-piglow.py
+++ gmail-notify-piglow.py
@@ -15,9 +15,8 @@ import time
 import feedparser
 
 ## global variables for gmail
-## username = raw_input('Your gmail username is:')
-username789 = "EMAIL@gmail.com"
-password = "PASSWORD"
+username = raw_input("Your gmail username is: ")
+password = raw_input ("Your password is: ")
 mail = int(feedparser.parse("https://" + username + ":" + password +
 "@mail.google.com/gmail/feed/atom/")["feed"] ["fullcount"])
 contact = int(feedparser.parse("https://" + username + ":" + password +
