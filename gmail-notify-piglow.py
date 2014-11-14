3be9c19ac24cab15177a3c09528f08ecd882becc
Dan <dandandrumsgm@gmail.com>
2014-11-14 18:02:25 +0000

    i wrote this code on a train without internet. it's so bad.
    save yourself and just don't read the changes. it's horrible.

--- gmail-notify-piglow.py
+++ gmail-notify-piglow.py
@@ -3,6 +3,10 @@
 ##Family Py!######  ##Edition###
 ##################  ############
 
+################################
+##I'm on a train woooWooooo#####
+################################
+
 ## Author: Daniel Pullan
 ## Twitter: @soundsarenoisy
 ## Github: /soundsarenoisy
@@ -30,10 +34,45 @@ number = 0
 val = 100
 speedval = 90
 
-if mail > 0: 
+
+## Option chooser. It may not be pretty, it may not even work,
+## I did it myself without looking anything up. Airplane mode wooo.
+option = raw_input("Pick an option [1,2,3] for version. 1 - easy, 
+2 - light per email, 3 - with contact mode")
+
+if option == 1:
+	easyMode
+	break
+
+elif option == 2: 
+	hardMode
+	break
+
+elif option == 3:
+	print("Refer to readme to setup contact function")
+	contactMode
+	break
+
+else:
+	print("Come on man, we spoke about this. Try again friend.")
+	break
+	
+## Contact Mode
+if true:
+	print("You have " + mail + " messages in inbox and " + contact
+	+ " are from your Contact.")
+
+## Easy Mode
+if mail > 0
+	print("You got mail buddy")
+	break
+
+
+## Hard Mode
+if mail == 1: 
         pyglow.all(0)
         pyglow.pulse(1, 100, speedval)
-        print('Item has sold!')
+        print('Item has sold')
 
 elif mail > 4:
         pyglow.all(0)
