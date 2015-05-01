3fea0928365e31660d79b6d01bff5130a513c9f1
Dan <dandandrumsgm@gmail.com>
2015-05-01 22:33:49 +0100

    Added unread email count
    nearly uploaded my email + password in my last commit. nearly.

--- gmail-send-notify.py
+++ gmail-send-notify.py
@@ -14,17 +14,13 @@ print "Please remember to have your Google account setup to allow login from non
 print "I will look into fixing this in a future update."
 
 ## variables
-username = raw_input("gmail address: ")
-password = raw_input("password: ")
 mail = int(feedparser.parse("https://" + username + ":" + password +
 "@mail.google.com/gmail/feed/atom/")["feed"] ["fullcount"])
+mail2 = ("You have " + str(mail) + " messages")
 
-Hello=Notify.Notification.new ("Hello world","you have 21 messages" ,"emblem-default")
-Goodbye=Notify.Notification.new ("Hello world","you have less than 21 messages" ,"emblem-default")
-
-messagesLow=Notify.Notification.new ("Few messages","You have a few messages." ,"emblem-default")
-messagesMed=Notify.Notification.new ("Lots of messages","You have quite a few messages." ,"emblem-default")
-messagesHigh=Notify.Notification.new ("Mayday, Mayday","You have a tonne of messages." ,"emblem-default")
+messagesLow=Notify.Notification.new ("Few messages", mail2 ,"emblem-default")
+messagesMed=Notify.Notification.new ("Lots of messages",mail2 ,"emblem-default")
+messagesHigh=Notify.Notification.new ("Mayday, Mayday",mail2 ,"emblem-default")
 
 
 ## Big red button.
