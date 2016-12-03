2fb29f3942ae3344cb34d8dcc0bf3c3cbbc86733
Daniel Pullan <dandandrumsgm@gmail.com>
2016-12-03 23:47:14 +0000

    added piglow, removed pyglow refs, added licence
    piglow was written by @Boeeerb

--- gmn_piglow.py
+++ gmn_piglow.py
@@ -3,35 +3,35 @@
 ## Website: DanielPullan.co.uk
 ## Device: Pimoroni PiGlow
 
-from PyGlow import PyGlow
+from piglow import PiGlow
 import pygame
 from time import sleep
-import feedparser
-from login import login
+import login
+from login import mail
 
 ## Init the current device
-pyglow = PyGlow()
+piglow = PiGlow()
 
 ## Device parameters
 b = 100
 s = 1000
-pyglow = PyGlow(brightness=int(b), speed=int(s), pulse=True)
-pyglow.all(0)
+piglow = PiGlow(brightness=int(b), speed=int(s), pulse=True)
+piglow.all(0)
 
 if mail < 5:
         print "mail count low"
-        pyglow.arm1(10)
+        piglow.arm1(10)
 elif mail < 10:
         print "mail count medium"
-        pyglow.arm1(10)
-        pyglow.arm2(10)
+        piglow.arm1(10)
+        piglow.arm2(10)
 elif mail < 15:
         print "mail count high"
-        pyglow.arm1(10)
-        pyglow.arm2(10)
+        piglow.arm1(10)
+        piglow.arm2(10)
         piglow.arm3(10)
 else:
         print "too many emails"
         for x in range(0, 10):
-                pyglow.all(100)
+                piglow.all(100)
                 sleep(0.5)
--- login.py
+++ login.py
@@ -2,4 +2,11 @@
 ## Github: GitHub.com/DanielPullan
 ## Website: DanielPullan.co.uk
 
-## This is the login script. 
+## This is the login script.
+
+import feedparser
+
+username = raw_input("Your gmail username is: ")
+password = raw_input ("Your password is: ")
+mail = int(feedparser.parse("https://" + username + ":" + password +
+"@mail.google.com/gmail/feed/atom/")["feed"] ["fullcount"])
