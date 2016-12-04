1834d8c51d2087bb1930545c686df4c88239bd88
Daniel Pullan <dandandrumsgm@gmail.com>
2016-12-04 15:41:45 +0000

    added sense support

---
+++ gmn_sense.py
@@ -0,0 +1,14 @@
+## Author: Daniel Pullan
+## Github: GitHub.com/DanielPullan
+## Website: DanielPullan.co.uk
+## Device: Pimoroni PiGlow
+
+from sense_emu import SenseHat
+from time import sleep
+import login
+from login import mail
+
+## Init the current device
+from sense_emu import SenseHat
+
+sense.show_message(mail, text_colour=[255, 0, 0])
