3ce1495e3bbb7001f22378ce8dc6e6da9540a8a3
Daniel Pullan <dandandrumsgm@gmail.com>
2016-12-04 15:38:11 +0000

    Revert "removed pygame from piglow, starting Sense support"
    This reverts commit ccb5d835caa2f214d827dfd3d999725d3089eb2d.

--- gmn_piglow.py
+++ gmn_piglow.py
@@ -1,16 +1,41 @@
 ## Author: Daniel Pullan
 ## Github: GitHub.com/DanielPullan
 ## Website: DanielPullan.co.uk
-## Device: Sense HAT / Astro Pi
+## Device: Pimoroni PiGlow
 
-from sense_emu import SenseHat
+from piglow import PiGlow
+import pygame # needed? might be from older bosrd
 from time import sleep
 import login
 from login import mail
 
 ## Init the current device
-sense = SenseHat()
+piglow = PiGlow()
 
 ## Device parameters
+piglow.all(0)
 
-sense.show_message(mail, text_colour=[255, 0, 0])
+if mail == 0:
+        print "no mail"
+        piglow.all(0)
+        sleep(0.5)
+elif mail < 5:
+        print "mail count low"
+        piglow.arm1(10)
+        sleep(0.5)
+elif mail < 10:
+        print "mail count medium"
+        piglow.arm1(10)
+        piglow.arm2(10)
+        sleep(0.5)
+elif mail < 15:
+        print "mail count high"
+        piglow.arm1(10)
+        piglow.arm2(10)
+        piglow.arm3(10)
+        sleep(0.5)
+else:
+        print "too many emails"
+        for x in range(0, 10):
+                piglow.all(100)
+                sleep(0.5)
