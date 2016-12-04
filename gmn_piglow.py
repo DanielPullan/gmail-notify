ca937e93c68c4015a379e6c4f91f04412accf50e
Daniel Pullan <dandandrumsgm@gmail.com>
2016-12-04 15:13:24 +0000

    basics of piglow working

--- gmn_piglow.py
+++ gmn_piglow.py
@@ -13,23 +13,23 @@ from login import mail
 piglow = PiGlow()
 
 ## Device parameters
-b = (100)
-s = (1000)
-piglow = PiGlow(brightness=int(b), speed=int(s), pulse=True)
 piglow.all(0)
 
 if mail < 5:
         print "mail count low"
         piglow.arm1(10)
+        sleep(0.5)
 elif mail < 10:
         print "mail count medium"
         piglow.arm1(10)
         piglow.arm2(10)
+        sleep(0.5)
 elif mail < 15:
         print "mail count high"
         piglow.arm1(10)
         piglow.arm2(10)
         piglow.arm3(10)
+        sleep(0.5)
 else:
         print "too many emails"
         for x in range(0, 10):
