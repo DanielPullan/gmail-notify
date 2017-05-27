bc52569eb305f564f46f09fc35e138e116f974b4
Daniel Pullan <dandandrumsgm@gmail.com>
2017-05-27 22:12:47 +0100

    added comments to gmn-simplepush
    i want to show that i can follow good practice when coding

--- gmn-simplepush.py
+++ gmn-simplepush.py
@@ -7,20 +7,25 @@
 # i had to pip-install simplepush, if you have issues, installing python-dev
 # fixed it for me
 from simplepush import send_encrypted
+import feedparser
 # i made a config file defining key, password and salt so that I could upload
 # it once and then gitignore it to avoid encryption keys being uploaded
-import feedparser
 from  config import email, password, salt, key
 
+## create an int using feedparser to parse the fullcount from the email feed
 mail = int(feedparser.parse("https://" + email + ":" + password +
 "@mail.google.com/gmail/feed/atom/")["feed"] ["fullcount"])
 
-
+## create a function called alert
 def alert():
+        ## use send_encrypted from simplepush to send an alert
         send_encrypted (key, password, salt, "Email", "New email")
 
-
+## If there is an email at all (more than nothing)
 if mail > 0:
+        ## do the alert function
         alert()
+## else (there is no emails)
 else:
+        ## print there is nothing, mainly for logging purposes
         print ("There is nothing")
